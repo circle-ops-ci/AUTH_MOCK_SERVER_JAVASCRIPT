462e82150aec5bbd7be993157eb26f1a7f4d2ad0
JoeLee <joe@cybavo.com>
2020-04-23 18:23:54 +0800

    Init

---
+++ helper/apicaller.js
@@ -0,0 +1,83 @@
+const https = require('https');
+const crypto = require('crypto');
+const rs = require('./randstr');
+const cfg = require('../models/config');
+
+function buildChecksum(params, secret, t, r, postData) {
+  const p = params || [];
+  p.push(`t=${t}`, `r=${r}`);
+  if (!!postData) {
+    p.push(postData);
+  }
+  p.sort();
+  p.push(`secret=${secret}`);
+  return crypto.createHash('sha256').update(p.join('&')).digest('hex');
+}
+
+function tryParseJSON(s) {
+  try {
+    const o = JSON.parse(s);
+    if (o && typeof o === 'object') {
+      return o;
+    }
+  } catch (e) {}
+  return s;
+}
+
+function doRequest(url, options, postData) {
+  console.log(`request -> ${url}, options -> `, options, `, postData -> ${postData}`);
+  return new Promise((resolve, reject) => {
+    let req = https.request(url, options, (res) => {
+      let resData = [];
+      res.on('data', (fragments) => {
+        resData.push(fragments);
+      });
+      res.on('end', () => {
+        let resBody = Buffer.concat(resData);
+        resolve({ result: tryParseJSON(resBody.toString()), statusCode: res.statusCode });
+      });
+      res.on('error', (error) => {
+        reject(error);
+      });
+    });
+    req.on('error', (error) => {
+      reject(error);
+    });
+    if (!!postData) {
+      req.write(postData);
+    }
+    req.end();
+  });
+}
+
+module.exports.makeRequest = async function (method, api, params, postData) {
+  if (method === '' || api === '') {
+    return { error: 'invalid parameters' };
+  }
+  const r = rs.randomString(8);
+  const t = Math.floor(Date.now()/1000);
+  let url = `${cfg.api_server_url}${api}?t=${t}&r=${r}`;
+  if (!!params) {
+    url += `&${params.join('&')}`;
+  }
+  const options = {
+    method,
+    headers: {
+      'X-API-CODE': cfg.api_code,
+      'X-CHECKSUM': buildChecksum(params, cfg.api_secret, t, r, postData)
+    },
+  };
+  if (method === 'POST' || method === 'DELETE') {
+    options.headers['Content-Type'] = 'application/json';
+    if (!!postData.length) {
+      options.headers['Content-Length'] = postData.length;
+    }
+  }
+
+  try {
+    let result = await doRequest(url, options, postData);
+    return tryParseJSON(result);
+  } catch(error) {
+    return tryParseJSON(error);
+  }
+}
---
+++ routes/api.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const api = require('../helper/apicaller');
+const cfg = require('../models/config');
+const crypto = require('crypto');
+const router = express.Router();
+
+function getQueryParams(query) {
+  return Object.keys(query).map((key) => {
+    return `${key}=${query[key]}`;
+  }); 
+}
+
+router.post('/users', async function(req, res) {
+  const apires = await api.makeRequest("POST", '/v1/api/users',
+    null, JSON.stringify(req.body));
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.post('/devices', async function(req, res) {
+  const apires = await api.makeRequest("POST", '/v1/api/devices',
+    getQueryParams(req.query), null);
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.post('/users/pin', async function(req, res) {
+  const apires = await api.makeRequest("POST", '/v1/api/users/pin',
+    getQueryParams(req.query), null);
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.get('/devices', async function(req, res) {
+  const apires = await api.makeRequest("GET", '/v1/api/devices',
+    getQueryParams(req.query), null);
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.delete('/devices', async function(req, res) {
+  const apires = await api.makeRequest("DELETE", '/v1/api/devices',
+    getQueryParams(req.query), JSON.stringify(req.body));
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.post('/devices/2fa', async function(req, res) {
+  const apires = await api.makeRequest("POST", '/v1/api/devices/2fa',
+    getQueryParams(req.query), JSON.stringify(req.body));
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.get('/users/2fa', async function(req, res) {
+  const apires = await api.makeRequest("GET", '/v1/api/users/2fa',
+    getQueryParams(req.query), null);
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.delete('/users/2fa/:token', async function(req, res) {
+  const apires = await api.makeRequest("DELETE", `/v1/api/users/2fa/${req.params.token}`,
+    getQueryParams(req.query), null);
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.get('/users/me', async function(req, res) {
+  const apires = await api.makeRequest("GET", '/v1/api/users/me',
+    getQueryParams(req.query), null);
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.post('/order/status', async function(req, res) {
+  const apires = await api.makeRequest("POST", '/v1/api/order/status',
+    getQueryParams(req.query), JSON.stringify(req.body));
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.post('/users/emailotp', async function(req, res) {
+  const apires = await api.makeRequest("POST", '/v1/api/users/emailotp',
+    getQueryParams(req.query), JSON.stringify(req.body));
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.get('/users/totpverify', async function(req, res) {
+  const apires = await api.makeRequest("GET", '/v1/api/users/totpverify',
+    getQueryParams(req.query), null);
+  if (apires.statusCode) {
+    res.status(apires.statusCode).json(apires.result);
+  } else {
+    res.status(400).json(apires);
+  }
+});
+
+router.post('/callback', function(req, res) {
+  console.log('callback ->', req.body);
+
+  const checksum = req.get('X-CHECKSUM');
+  const payload = JSON.stringify(req.body) + cfg.api_secret;
+  const buff = Buffer.from(crypto.createHash('sha256').update(payload).digest());
+  const checksumVerf = buff.toString('base64').replace(/\+/g, "-").replace(/\//g, "_");
+  if (checksum !== checksumVerf) {
+    res.status(400).send('Bad checksum');
+    return;
+  }
+  res.status(200).send('OK');
+});
+
+module.exports = router;
